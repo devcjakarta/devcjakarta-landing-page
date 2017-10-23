b46513ab1ede76868f12fef4325f8582a11ff0f4
Achmad Kurnianto <achmad.kurnianto@gmail.com>
2017-10-23 11:32:04 +0700

    add validation for occupation and institution field

--- src/utils/FormValidations.js
+++ src/utils/FormValidations.js
@@ -9,5 +9,7 @@ export function validateRegisterData (data) {
   if (!isEmail(data.email)) errors.email = (errors.email && errors.email.length) ? `${errors.email} dan valid` : "E-mail harus valid"
   if (isEmpty(data.phone)) errors.phone = "No telepon harus diisi"
   if (!(isMobilePhone(data.phone, 'id-ID') && (data.phone.length > 9) && (data.phone.length < 14))) errors.phone = (errors.phone && errors.phone.length) ? `${errors.phone} dan valid` : "No telepon harus valid"
+  if (isEmpty(data.institution)) errors.institution = "Institusi/Lembaga/Kantor harus diisi"
+  if (isEmpty(data.occupation)) errors.occupation = "Job Title/Major/Grade harus diisi"
   return errors
 }
